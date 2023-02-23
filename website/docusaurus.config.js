d682a458cd3d5b95716dd0786eff7b8f8935677c
Sirage-t <Sirage.tr@gmail.com>
2023-02-23 22:35:54 +1030

    fix trailingSlash

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   organizationName: 'Digital-Law-Lab', // Usually your GitHub org/user name.
   projectName: 'Digital-Law-Lab', // Usually your repo name.
   deploymentBranch: 'gh-pages',
-  trailingSlash: false,
+  trailingSlash: true,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',

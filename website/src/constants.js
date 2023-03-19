0ce4abd1b705f657e216b650f1943745413eb6cb
Mark Ferraretto <mark@ferraretto.com>
2023-03-19 14:38:56 +1030

    Display of links and constants works (at least, I think!) Not yet tested with a build

--- website/src/constants.js
+++ website/src/constants.js
@@ -21,13 +21,16 @@ export function ContactAdmin(props) {
     return <p>{contactText} <a href={dll_admin_url}>{dll_admin_name}</a></p>;
 }
 
-// Displays information about the Digital Law Lab administrator.  Of course,
-// This assumes there is only one administrator!  That could be a problem in
-// the future but isn't one now.
-// TODO: The 'full_url' displays a Markdown URL.  It does not get renedered as
-//       a URL but literally.  I've asked a question on Discord about this:
-//       https://discord.com/channels/398180168688074762/867060369087922187/1086550144431960154
+
 export function DisplayAdminData(props) {
+    // Expect: props.data - key to if statement (in body)
+    // Return: string with information requested
+    // Displays information about the Digital Law Lab administrator.  Of course,
+    // This assumes there is only one administrator!  That could be a problem in
+    // the future but isn't one now.
+    // TODO: The 'full_url' displays a Markdown URL.  It does not get renedered as
+    //       a URL but literally.  I've asked a question on Discord about this:
+    //       https://discord.com/channels/398180168688074762/867060369087922187/10865501444319601540
     if (props.data == 'name') {
         return dll_admin_name;
     } else if (props.data == 'url') {
@@ -41,17 +44,22 @@ export function DisplayAdminData(props) {
     }
 }
 
-// Incorporating the display of all cloud services into this Component.  At time
-// of writing this is:
-// - Docassemble dev server
-// - Docassemble prod server
-// - our Slack
-// - our GitHub
-// - our Google Drive
-//
-// What to display is in props.data
 export function DisplayServiceData(props) {
+    // Expect: props.data - key for service (as per switch statement in body)
+    //         props.service_name - name of service.  Is the name of the link returned
+    // Return: anchor tag with link to service
+    //
+    // Incorporating the display of all cloud services into this Component.  At time
+    // of writing this is:
+    // - Docassemble dev server
+    // - Docassemble prod server
+    // - our Slack
+    // - our GitHub
+    // - our Google Drive
+    //
+    // Assumes services available via web as returns anchor link
     let result = "";
+    let service_name = "";
     switch (props.data) {
         case 'dev_server':
             result = 'https://dev.dll.org.au/da/';
@@ -69,7 +77,12 @@ export function DisplayServiceData(props) {
             result = 'https://drive.google.com/drive/folders/1fK41cjHrIeqfOmdmi-0lF3dOtZAjgYf9';
             break;
         default:
-            result = 'DisplayServiceData: Incorrect data supplied';j
+            result = 'DisplayServiceData: Incorrect data supplied';
     }
-    return result;
+    if (props.service_name) {
+        service_name = props.service_name;
+    } else {
+        service_name = "No service defined";
+    }
+    return <span><a href={result}>{service_name}</a></span>
 }
